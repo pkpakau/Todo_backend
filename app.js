664fcd4f11ec13c3a34924ca2282cb7e7b365975
Parag Khuman <parag@server.example.com>
2019-01-05 13:02:01 +0530

    first update

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const cors=require("cors");
 
-const userController=require("./controllers/userController");
-
+const userController=require("./controllers/userController.js");
+const taskController=require("./controllers/taskController.js");
 const app=express();
 const port=3000;
 
-app.get('/',(req,res)=>{
-    res.send("Start page");
-})
+app.use('',userController);
 
+app.use('/dashboard',taskController);
+
+app.use(bodyParser.json());
 
 app.listen(port,()=>{
     console.log(`Server starated at port :${port}`);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,16 @@
 
 
+const express=require("express");
+
+
+const router=express.Router();
+
+router.get('/register',(req,res)=>{
+    res.send("Register Page");
+});
+
+
+router.get('/login',(req,res)=>{
+    res.send("Login Page");
+})
+module.exports=router;
